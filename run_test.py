c04e410701dd5f87e98adb712e7c1fbba7733d5a
qupan <34915305+qupan@users.noreply.github.com>
2018-09-06 11:40:38 +0800

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -6,5 +6,6 @@ import pytest,time
 if __name__=="__main__":
 	report_name=time.strftime('%Y_%m_%d-%H-%M-%S')
 	pytest.main(['-s','--html=report/html/%s report.html'%report_name,
+		'--self-contained-html',
 		'--resultlog=report/log/%s report.log'%report_name,
 		'--junitxml=report/xml/%s report.xml'%report_name])
