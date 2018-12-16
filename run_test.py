7fdde43905119325d5a2c56aa092ccfbf76145a5
qupan <34915305+qupan@users.noreply.github.com>
2018-12-16 15:13:55 +0800

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -1,11 +1,17 @@
-#coding=utf-8
+# -*- coding:utf-8 -*-
 import pytest,time
-'''
-运行所有用例
-'''
+
+'''运行所有用例，并生成html，tap，xml报告'''
+
 if __name__=="__main__":
-	report_name=time.strftime('%Y_%m_%d-%H-%M-%S')
-	pytest.main(['-s','--html=report/html/%s report.html'%report_name,
-		'--self-contained-html',
-		'--resultlog=report/log/%s report.log'%report_name,
-		'--junitxml=report/xml/%s report.xml'%report_name])
+	name=time.strftime('%Y_%m_%d-%H-%M-%S')
+	pytest.main(['-q',#简化输出信息，保留核心内容
+		'-s',#运行所有的用例
+		'--html=report/HTML/%s report.html'%name,#生成HTML格式报告
+		'--self-contained-html',#将HTML报告和css文件合并成一个
+		'--tap-outdir=report/TAP',#使用tap指出存放目录
+		'--tap-combined',#将所有tap合并存放
+		'--junitxml=report/XML/%s report.xml'%name,#生成xml文件
+		# '--count=2', #全部用例运行两遍
+		'--reruns=2',#失败的用例重新运行两遍
+		])
